cf04d5d7227c835f74827e37c6862c258768d348
Aniket Bharti <aniket.ars.bharti@gmail.com>
2023-01-29 20:30:54 +0530

    constants for holding static data

---
+++ lib/resources/consts.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primaryDark = Color(0xff420264);
+  static const Color primary = Color(0xff14213d);
+  static const Color primaryLight = Color(0xff14213d);
+  static const Color secondary = Color(0xff1B1734);
+  static const Color accent = Color(0xfffca311);
+  static const Color textColor = Color(0xffe9ecef);
+}
